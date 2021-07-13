b2da4c577e4ea4a3afc786e98083edeac59643bf
Lloyd Edano <ornel.lloyd.edano@gmail.com>
2021-07-13 19:22:02 +0400

    fixed class name type

--- src/main/scala/com/scalac/github_challenge/service/GitHubContributionService.scala
+++ src/main/scala/com/scalac/github_challenge/service/GitHubContributionService.scala
@@ -3,9 +3,15 @@ package com.scalac.github_challenge.service
 import scala.concurrent.Future
 import com.scalac.github_challenge.service.model.Failures._
 import com.scalac.github_challenge.service.model.{Contribution, Contributor, Organization}
+import com.scalac.github_challenge.util.ExecutionContextProvider
 
-class GithubContributionService extends DevContributionService {
-  override def getOrganizations(limit: Option[Int]): Future[Either[ServiceFailure, Seq[Organization]]] = ???
+class GitHubContributionService(ecProvider: ExecutionContextProvider) extends DevContributionService {
+  implicit val ec = ecProvider.ioBoundExecutionContext
+
+  //mock implementation
+  override def getOrganizations(limit: Option[Int]): Future[Either[ServiceFailure, Seq[Organization]]] = Future {
+    Right(Seq("Apple", "Google", "Microsoft", "Netflix", "Oracle").map(Organization(_)))
+  }
   override def getContributors(organization: Organization, limit: Option[Int]): Future[Either[ServiceFailure, Seq[Contributor]]] = ???
   override def getContributions(contributor: Contributor, organization: Organization): Future[Either[ServiceFailure, Contribution]] = ???
 }
