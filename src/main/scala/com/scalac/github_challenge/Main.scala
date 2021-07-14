db267abed4793146e176f9fd60b321d72c8e971b
Lloyd Edano <ornel.lloyd.edano@gmail.com>
2021-07-14 22:46:32 +0400

    -remove not needed implicit converter -changed to classic actor system to launch http server

--- src/main/scala/com/scalac/github_challenge/Main.scala
+++ src/main/scala/com/scalac/github_challenge/Main.scala
@@ -1,14 +1,13 @@
 package com.scalac.github_challenge
 
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import com.scalac.github_challenge.api.Controller
 import com.scalac.github_challenge.service.GitHubContributionService
 import com.scalac.github_challenge.util.ExecutionContexts
 
 object Main extends App {
-  implicit val system = ActorSystem(Behaviors.empty, "scalac-github-challenge")
+  implicit val system = ActorSystem("scalac-github-challenge")
   val controller = new Controller(new GitHubContributionService(ExecutionContexts))
   Http().newServerAt("localhost", 8080).bind(controller.routes)
 }
