10e6f8c390b63a55b8770b5458b11bf74b9380ec
Lloyd Edano <ornel.lloyd.edano@gmail.com>
2021-07-14 23:00:02 +0400

    -updated controller, added and removed some routes

--- src/main/scala/com/scalac/github_challenge/api/Controller.scala
+++ src/main/scala/com/scalac/github_challenge/api/Controller.scala
@@ -6,10 +6,10 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.scalac.github_challenge.api.format.JsonFormat
 import com.scalac.github_challenge.service.DevContributionService
-import com.scalac.github_challenge.service.model.{Contributor, Organization}
+import com.scalac.github_challenge.service.model.{Organization}
 
 import scala.util.{Failure, Success}
-import com.scalac.github_challenge.service.model.Failures.{ContributorNotFound, OrganizationNotFound}
+import com.scalac.github_challenge.service.model.Failures.{OrganizationNotFound}
 import com.scalac.github_challenge.mapping.Implicits._
 
 class Controller(contributionService: DevContributionService) extends JsonFormat with SprayJsonSupport {
@@ -22,37 +22,39 @@ class Controller(contributionService: DevContributionService) extends JsonFormat
           onComplete(contributionService.getOrganizations(limit)) {
             case Success(Right(result))=>
               complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, result.map(_.name).toJson.prettyPrint))
-            case Failure(exception: Exception)=>
-              complete(StatusCodes.InternalServerError, exception.getMessage)
-          }
-        }
-      } ~
-      path("orgs" / Segment / "contributors") { orgName=>
-        get {
-          onComplete(contributionService.getContributors(Organization(orgName), limit)) {
-            case Success(Right(result))=>
-              complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, result.map(_.name).toJson.prettyPrint))
-            case Success(Left(OrganizationNotFound(name)))=>
-              complete(StatusCodes.NotFound, s"Organization [$name] was not found.")
-            case Failure(exception: Exception)=>
+            case Failure(exception)=>
               complete(StatusCodes.InternalServerError, exception.getMessage)
           }
         }
       }
     } ~
-    path("orgs" / Segment / "contributors" / Segment) { (orgName, devName)=>
+  path("orgs" / Segment / "repos") { orgName=>
+    parameter('limit.as[Int].optional) { (limit: Option[Int])=>
       get {
-        onComplete(contributionService.getContributions(Contributor(devName), Organization(orgName))) {
+        onComplete(contributionService.getRepos(Organization(orgName), limit)) {
           case Success(Right(result))=>
-            complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, result.toApi.toJson.prettyPrint))
+            complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, result.map(_.name).toJson.prettyPrint))
           case Success(Left(OrganizationNotFound(name)))=>
             complete(StatusCodes.NotFound, s"Organization [$name] was not found.")
-          case Success(Left(ContributorNotFound(name)))=>
-            complete(StatusCodes.NotFound, s"Contributor [$name] was not found.")
-          case Failure(exception: Exception)=>
+          case Failure(exception)=>
             complete(StatusCodes.InternalServerError, exception.getMessage)
         }
       }
     }
+  } ~
+  path("orgs" / Segment / "contributors") { orgName=>
+    parameter('num_repos.as[Int].optional) { (numRepos: Option[Int])=>
+      get {
+        onComplete(contributionService.getContributors(Organization(orgName), numRepos)) {
+          case Success(Right(result))=>
+            complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, result.map(_.toApi).toJson.prettyPrint))
+          case Success(Left(OrganizationNotFound(name)))=>
+            complete(StatusCodes.NotFound, s"Organization [$name] was not found.")
+          case Failure(exception)=>
+            complete(StatusCodes.InternalServerError, exception.getMessage)
+        }
+      }
+    }
+  }
 
 }
