ca6ba6c24154b672794cb0b464df9ac6ce75765c
Lloyd Edano <ornel.lloyd.edano@gmail.com>
2021-07-15 17:55:52 +0400

    -added request response logging in the controller -added config to ignore akka http warning

--- src/main/scala/com/scalac/github_challenge/Main.scala
+++ src/main/scala/com/scalac/github_challenge/Main.scala
@@ -2,12 +2,14 @@ package com.scalac.github_challenge
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.stream.Materializer
 import com.scalac.github_challenge.api.Controller
 import com.scalac.github_challenge.service.GitHubContributionService
 import com.scalac.github_challenge.util.{AkkaHttpClient, Configs, ExecutionContexts}
 
 object Main extends App {
   implicit val system = ActorSystem("scalac-github-challenge")
+  implicit val mat = Materializer
   implicit val ec = ExecutionContexts
   val controller = new Controller(new GitHubContributionService(new AkkaHttpClient, Configs))
   Http().newServerAt("localhost", 8080).bind(controller.routes)
--- src/main/scala/com/scalac/github_challenge/api/Controller.scala
+++ src/main/scala/com/scalac/github_challenge/api/Controller.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.stream.Materializer
 import com.scalac.github_challenge.api.format.JsonFormat
 import com.scalac.github_challenge.service.DevContributionService
 import com.scalac.github_challenge.service.model.Organization
@@ -11,11 +12,12 @@ import com.scalac.github_challenge.service.model.Organization
 import scala.util.{Failure, Success}
 import com.scalac.github_challenge.service.model.Failures.{ExternalCallError, OrganizationNotFound}
 import com.scalac.github_challenge.mapping.Implicits._
+import com.scalac.github_challenge.util.ExecutionContextProvider
 
-class Controller(contributionService: DevContributionService) extends JsonFormat with SprayJsonSupport {
+class Controller(contributionService: DevContributionService)(implicit val mat: Materializer) extends JsonFormat with SprayJsonSupport with RouteLogging {
   import spray.json._
 
-  val routes: Route =
+  val routes: Route = logRequestResponse {
     parameter('limit.as[Int].optional) { (limit: Option[Int])=>
       path("orgs") {
         get {
@@ -61,6 +63,7 @@ class Controller(contributionService: DevContributionService) extends JsonFormat
         }
       }
     }
+  }
   }
 
 }
